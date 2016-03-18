5bc4b95a87d029f76e3547167f1882e68a7b7d53
himanshu-hearthacker <himkiet@gmail.com>
2016-03-19 00:48:04 +0545

    adding ejs templating engine

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 var express = require('express');
-var  morgan = require('morgan');
+var morgan = require('morgan');
 var app = express();
 var mongoose = require('mongoose');
 var User = require('./models/user');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
 
 
 // middleware
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(morgan('dev'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
-app.post('/create-user', function(req, res, next){
+app.post('/create-user', function(req, res, next) {
 	var user = new User();
 	console.log(req.body.name);
 	user.profile.name = req.body.name;
 	user.password = req.body.password;
 	user.email = req.body.email;
-	
-	user.save(function(err){
+
+	user.save(function(err) {
 		if (err) return next(err);
 		res.json('succesfully created a new user');
 	});
@@ -28,14 +34,22 @@ app.post('/create-user', function(req, res, next){
 
 });
 
-mongoose.connect('mongodb://root:123@ds015929.mlab.com:15929/e-commerce', function(err){
- if (err){
-console.log(err);
- }else{console.log("connected to database"); }
+app.get('/', function(req, res) {
+
+	res.render('home');
+
+});
+
+mongoose.connect('mongodb://root:123@ds015929.mlab.com:15929/e-commerce', function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("connected to database");
+	}
 });
 
 
-app.listen(3000,function (err) {
+app.listen(3000, function(err) {
 	if (err) throw err;
 	console.log("server is running ");
 });
